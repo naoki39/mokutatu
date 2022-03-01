ec217255788f181837ff28427dd242f6f52b9904
naoki39 <naokibad0311@gmail.com>
2022-03-01 14:23:33 +0900

    ユーザー新規登録の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+       
+  with_options presence: true do
+    validates :image
+    validates :nickname
+    validates :goal, format: { with: /(?=.*\d)/ }
+    validates :second_goal, format: { with: /(?=.*\d)/ }
+    validates :to_do, format: { with: /(?=.*\d)/ }
+  end
+
+  has_one_attached :image     
 end
