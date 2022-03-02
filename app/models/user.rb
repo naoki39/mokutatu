34a7140fb5e2f1c63326718ec3cc1c388cde10d5
naoki39 <naokibad0311@gmail.com>
2022-03-02 18:06:28 +0900

    コードが消えてしまったので修正

---
+++ app/models/user.rb
@@ -0,0 +1,21 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+         validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+
+         with_options presence: true do
+           validates :nickname
+           validates :goal, format: { with: /(?=.*\d)/ }
+           validates :second_goal, format: { with: /(?=.*\d)/ }
+           validates :to_do, format: { with: /(?=.*\d)/ }
+           validates :image
+         end
+  
+  has_one_attached :image
+  has_many :community_users
+  has_many :communities, through: :community_users
+       
+end
