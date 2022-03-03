bce248fc1af67d33b72881ab9c2ee9c71443e06e
naoki39 <naokibad0311@gmail.com>
2022-03-03 14:46:05 +0900

    postコントローラーの記述

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,23 @@
 class PostsController < ApplicationController
+#before_action :authenticate_user!
+
   def index
+   @posts = Post.all
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    Post.create(post_params)
+  end
+
+  def show
   end
 
   private
-  
+  def post_params
+    params.require(:post).permit(:text).merge(user_id: current_user.id)
+  end
 end
